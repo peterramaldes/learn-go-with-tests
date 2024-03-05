d89afd4715d7b81b9451bfc41658abbdd4adf2b0
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-03-05 07:20:18 -0300

    Hello World if Empty String

--- 001_hello_world/hello_test.go
+++ 001_hello_world/hello_test.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Peter")
+		want := "Hello, Peter"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
 
 func ExampleHello() {
 	fmt.Println(Hello("Peter"))
