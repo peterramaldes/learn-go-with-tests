0c4d081c09746dbb8e6691fd2bf460152f53870c
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-03-06 06:42:45 -0300

    Refactor to `greetingPrefix()`

--- 001_hello_world/hello.go
+++ 001_hello_world/hello.go
@@ -4,20 +4,33 @@ import (
 	"fmt"
 )
 
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
+	return greetingPrefix(language) + name
+}
 
-	return englishHelloPrefix + name
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
--- 001_hello_world/hello_test.go
+++ 001_hello_world/hello_test.go
@@ -21,6 +21,11 @@ func TestHello(t *testing.T) {
 		want := "Hola, Odin"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Odin", "French")
+		want := "Bonjour, Odin"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
