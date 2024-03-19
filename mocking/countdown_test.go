d38cdca0f5f61b9856ee65b42b75b0f15fc1173c
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-03-19 07:51:30 -0300

    Spying during the test
    If we want to know the order, this is the right way to spy and collect the data.

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -13,24 +14,63 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+const write = "write"
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func TestCountdown(t *testing.T) {
-	b := &bytes.Buffer{}
-	s := &SpySleeper{}
+	t.Run("test countdown without order", func(t *testing.T) {
+		b := &bytes.Buffer{}
+		s := &SpySleeper{}
 
-	Countdown(b, s)
+		Countdown(b, s)
 
-	got := b.String()
-	want := `3
+		got := b.String()
+		want := `3
 2
 1
 Go!
 `
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if s.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", s.Calls)
-	}
+		if s.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", s.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		s := &SpyCountdownOperations{}
+		Countdown(s, s)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, s.Calls) {
+			t.Errorf("wanted calls %v got %v", want, s.Calls)
+		}
+	})
+
 }
