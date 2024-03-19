59c116d658d701c0061c052682bb5ed3eaafccda
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-03-19 07:01:38 -0300

    Added `update` functionality

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,6 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExists = errors.New("the word that you are trying to add it's already in the dictionary")
 
 func (d Dictionary) Search(word string) (string, error) {
 	def, ok := d[word]
@@ -16,10 +11,43 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 func (d Dictionary) Add(word, definition string) error {
-	_, ok := d[word]
-	if ok {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
 		return ErrWordExists
+	default:
+		return err
 	}
-	d[word] = definition
+
 	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("the word that you are trying to add it's already in the dictionary")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
 }
