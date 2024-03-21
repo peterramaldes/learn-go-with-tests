5284c4caac1397e11c2c5480654b15072ed80991
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-03-21 07:18:36 -0300

    Concurrency & Benchmarking

--- concurrency/check_websites.go
+++ concurrency/check_websites.go
@@ -2,11 +2,24 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
-func CheckWebsites(wb WebsiteChecker, urls []string) map[string]bool {
-	results := make(map[string]bool)
+type result struct {
+	string
+	bool
+}
 
-	for _, u := range urls {
-		results[u] = wb(u)
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
